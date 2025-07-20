0d2216a3e892f2faf01af1b8ecca1510bc8bae82
AravindMuthusamy1306 <aravindmuthusamy188@gmail.com>
2025-07-20 10:49:29 +0530

    HTTP Request and Endpoint Example file created

--- Express.JS/index.js
+++ Express.JS/index.js
@@ -3,8 +3,14 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hi!!');
 });
+app.get('/about', (req,res) => {
+    res.send('About Me: I am a Software Developer');
+});
+app.get('/contact', (req,res) =>{
+    res.send('Hello! You can contact me at @aravindmuthusamy_');
+})
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
