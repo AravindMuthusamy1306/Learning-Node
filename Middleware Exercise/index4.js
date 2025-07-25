e7caba08ec7dbe3531c83f93a3d02bea4c8969cf
AravindMuthusamy1306 <aravindmuthusamy188@gmail.com>
2025-07-25 13:08:21 +0530

    Middleware Exercise

--- Middleware Exercise/index4.js
+++ Middleware Exercise/index4.js
@@ -6,15 +6,23 @@ import bodyParser from "body-parser";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+let bandName = "";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+function bandNameGenerator(req, res, next){
+  console.log(req.body);
+  bandName = req.body.street + " " + req.body.pet;
+  next();
+}
+app.use(bandNameGenerator);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/submit", (req, res) => {
-  res.send("<h1> Your Band Name is " + req.body.street + req.body.pet + "</h1>");
+  res.send(`<h1>Your band name is: ${bandName}</h1>
+            <a href="/">Go back</a>`);
 });
 
 app.listen(port, () => {
