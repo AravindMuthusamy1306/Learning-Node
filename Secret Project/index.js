713b53d7758aea588a09fa22660948def9535745
AravindMuthusamy1306 <aravindmuthusamy188@gmail.com>
2025-07-26 07:23:33 +0530

    Secret Project Created

---
+++ Secret Project/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const PORT = 3000;
+
+let userAuthenticated = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+function authenticateUser(req, res, next) {
+    const password = req.body.secret;
+  if (password === 'Suv@2oo4') {
+    userAuthenticated = true;
+    next();
+  }
+  else{
+    res.status(401).send('<h1>Unauthorized: Incorrect password</h1>');
+  }
+}
+
+app.post('/secret', authenticateUser, (req, res) => {
+  res.sendFile(__dirname + '/public/secret.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
