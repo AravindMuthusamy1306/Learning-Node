0f71f0d03773db0d98eff2d8f9a5545bf7d963b3
AravindMuthusamy1306 <aravindmuthusamy188@gmail.com>
2025-07-30 07:45:38 +0530

    Minor changes

--- EJS Exercise 2/index.js
+++ EJS Exercise 2/index.js
@@ -4,16 +4,18 @@ import bodyParser from 'body-parser';
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', { fname: '', lname: '' });
+    res.render('index', { fname: '', lname: '' });
 });
 
 app.post('/submit', (req, res) => {
     const fname = req.body.fname;
     const lname = req.body.lname;
-    res.render('index.ejs', { fname, lname });
+    res.render('index', { fname, lname });
 });
 
 app.listen(PORT, () =>{
