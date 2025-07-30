3a89a2232b1223766b81ed9b15373e7158e40755
AravindMuthusamy1306 <aravindmuthusamy188@gmail.com>
2025-07-30 07:38:19 +0530

    EJS Exercise 2 Completed

---
+++ EJS Exercise 2/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { fname: '', lname: '' });
+});
+
+app.post('/submit', (req, res) => {
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    res.render('index.ejs', { fname, lname });
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
